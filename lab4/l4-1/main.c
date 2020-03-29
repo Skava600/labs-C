76ab1012d752a4d961072860b19f4fd8b6bb0520
Skava600 <56450477+Skava600@users.noreply.github.com>
2020-03-29 20:59:59 +0300

    Add files via upload

---
+++ lab4/l4-1/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    printf("Enter text: ");
+    gets(str);
+    for(int i=0;str[i]!=NULL;i++)
+    {
+        printf("%c",str[i]);
+        if((str[i] == ',' || str[i] == ':' || str[i] == '.' || str[i] == ';')  && str[i+1] != ' ')
+        {
+            printf(" ");
+        }
+    }
+    return 0;
+}
