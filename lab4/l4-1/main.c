a08a862fbfb0a2dcf485226ba78efea80b2c802b
Skava600 <56450477+Skava600@users.noreply.github.com>
2020-03-29 21:37:10 +0300

    Update main.c

--- lab4/l4-1/main.c
+++ lab4/l4-1/main.c
@@ -5,10 +5,10 @@ int main()
     char str[100];
     printf("Enter text: ");
     gets(str);
-    for(int i=0;str[i]!=NULL;i++)
+    for(int i = 0; str[i] != NULL; i++)
     {
         printf("%c",str[i]);
-        if((str[i] == ',' || str[i] == ':' || str[i] == '.' || str[i] == ';')  && str[i+1] != ' ')
+        if( (str[i] == ',' || str[i] == ':' || str[i] == '.' || str[i] == ';' )  && (str[i+1] != ' ') )
         {
             printf(" ");
         }
