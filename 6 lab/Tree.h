2d6416916a23a4e0dccdecba86bf126f63651ed1
Skava600 <56450477+Skava600@users.noreply.github.com>
2020-05-13 18:50:54 +0300

    Update and rename tree.h to Tree.h

--- 6 lab/Tree.h
+++ 6 lab/Tree.h
@@ -5,7 +5,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-#include "que.h"
+
+struct Queue
+{
+    int capacity, size, rear, front;
+    int* array;
+    /*Указатели на адреса начала списка и его конца*/
+};
+struct Queue* createQueue( int capacity);
+int isEmpty(struct Queue* queue) ;
+void enQueue(struct Queue* queue, int item) ;
+int deQueue(struct Queue* queue);
+int front(struct Queue* queue);
+int rear(struct Queue* queue) ;
+void showArray(struct Queue* queue);
+
 typedef struct tree
 {
     int field;
@@ -20,5 +34,4 @@ void postorder(tnode *root);
 void inorder(tnode *root);
 tnode* cutTree( tnode *tree);
 int isLeaf( tnode *node) ;
-//struct tnode* createTree(void);
 #endif
